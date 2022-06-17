549c8982ba49aaeae34f53580ced9ae1091348e9
Om Prakash Banerjee <omprakashbanerjeee@gmail.com>
2022-06-17 17:12:08 +0530

    creating file added

--- rust-reading_a_file/src/main.rs
+++ rust-reading_a_file/src/main.rs
@@ -4,10 +4,24 @@ use std::io::prelude::*;
 
 
 fn main() {
+
+  /*reading a file */
   let mut file =File::open("textfile.txt").expect("Can't open file");
   let mut contents = String::new();
   file.read_to_string(&mut contents)
   .expect("oops cant read");
   println!("{}", contents);
+
+/*creating a file  */
+  let mut file = File::create("hello.txt").expect("Can't create file");
+  file .write_all(b"file created succeefully").expect("oops cant write");
+
+
+  /*Rad the file created */
+  let mut file = File ::open("hello.txt").expect("Can't open file");
+  let mut contents = String::new();
+  file.read_to_string(&mut contents).expect("cant read the hello file");
+  println!("hello file data is {}", contents);
+
     
 }
